35b633659065aec2d6e44c33a891c0209d3d8517
Stephen Muñez <sjtmunez@gmail.com>
2024-03-06 15:49:07 +0800

    feat: footer mobile

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import HeaderMobile from '../HeaderMobile';
+import Footer from '../Footer';
 
 const Layout = ({ children }) => {
   return (
     <>
       <HeaderMobile />
       {children}
+      <Footer />
     </>
   );
 };
