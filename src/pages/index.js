e25664197fbf318e8195028d9a12aa827cbf6c90
Stephen Muñez <sjtmunez@gmail.com>
2024-03-08 17:25:38 +0800

    feat: home services mobile

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,14 @@ import * as React from 'react';
 import Layout from '../components/Layout';
 import Seo from '../components/Seo';
 import HomeHero from '../components/HomeHero';
+import HomeServices from '../components/HomeServices';
 
 const Home = () => {
   return (
     <Layout>
       <main>
         <HomeHero />
+        <HomeServices />
       </main>
     </Layout>
   );
