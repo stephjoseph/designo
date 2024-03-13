b36908651f6c73ae0b9c47292eafe1ca5e7b5680
Stephen Muñez <sjtmunez@gmail.com>
2024-03-13 13:16:27 +0800

    setup: about data

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,16 +2,67 @@ import React from 'react';
 import Layout from '../components/Layout';
 import Seo from '../components/Seo';
 import AboutHero from '../components/AboutHero';
+import { graphql } from 'gatsby';
 
-const About = () => {
+const About = ({ data }) => {
   return (
     <Layout>
       <main>
-        <AboutHero />
+        <AboutHero data={data.aboutJson.hero} />
       </main>
     </Layout>
   );
 };
 
+export const query = graphql`
+  query {
+    aboutJson {
+      hero {
+        title
+        description
+        image {
+          desktop {
+            childImageSharp {
+              gatsbyImageData
+            }
+          }
+          mobile {
+            childImageSharp {
+              gatsbyImageData
+            }
+          }
+          tablet {
+            childImageSharp {
+              gatsbyImageData
+            }
+          }
+        }
+      }
+      subhero {
+        description
+        title
+        image {
+          desktop {
+            childImageSharp {
+              gatsbyImageData
+            }
+          }
+          mobile {
+            childImageSharp {
+              gatsbyImageData
+            }
+          }
+          tablet {
+            childImageSharp {
+              gatsbyImageData
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
 export const Head = () => <Seo title='About' />;
+
 export default About;
