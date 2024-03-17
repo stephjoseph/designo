0beabc6c1c1495243a57ca7f82fe5ac96f745944
Stephen Muñez <sjtmunez@gmail.com>
2024-03-17 11:10:12 +0800

    feat: home services tablet

--- src/components/HomeServices/HomeServices.js
+++ src/components/HomeServices/HomeServices.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useStaticQuery, graphql, Link } from 'gatsby';
 import { StaticImage } from 'gatsby-plugin-image';
 import { homeServices, homeServicesService } from './HomeServices.module.css';
 
 const HomeServices = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
   const data = useStaticQuery(graphql`
     query {
       allServicesJson {
@@ -29,15 +31,35 @@ const HomeServices = () => {
     }
   `);
 
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <section className={homeServices} id='services'>
       {data.allServicesJson.edges.map((item) => {
         const service = item.node;
+        let backgroundImageURL = service.image.mobile.publicURL;
+
+        if (windowWidth >= 768 && windowWidth < 1280) {
+          backgroundImageURL = service.image.tablet.publicURL;
+        } else if (windowWidth >= 1280) {
+          backgroundImageURL = service.image.desktop.publicURL;
+        }
+
         return (
           <div
             key={service.id}
             style={{
-              backgroundImage: `url(${service.image.mobile.publicURL})`,
+              backgroundImage: `url(${backgroundImageURL})`,
             }}
             className={homeServicesService}
           >
